b6adb427b710a0ffbf023ce0454c758de2557fb7
minheekangg <minheekangg@gmail.com>
2019-01-04 00:03:03 -0500

    added popup on login page

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Form, Button } from 'semantic-ui-react'
 import { Redirect } from 'react-router';
+import { Button, Popup, Form } from "semantic-ui-react";
 
 class Login extends React.Component {
   state = {
@@ -32,18 +32,21 @@ class Login extends React.Component {
 
 render(){
   if (this.state.loggedin) return <Redirect to={"/"} />;
-  return (
-    <div className="login-background">
+  return <div className="login-background">
       <div className="form-div">
-        <Form onSubmit={this.handleSubmitLogin} onChange={this.handleLoginChange} >
+        <Form onSubmit={this.handleSubmitLogin} onChange={this.handleLoginChange}>
           <h1>Please Login to start Learning!</h1>
-              <Form.Input type="text" name="username" placeholder="Username" value={this.state.username}  />
-              <Form.Input type="password" name="password" placeholder="Password" value={this.state.password}/>
-          <Button value="Login" > Login </Button>
-        </Form >
+          <Form.Input type="text" name="username" placeholder="Username" value={this.state.username} />
+          <Form.Input type="password" name="password" placeholder="Password" value={this.state.password} />
+          <Button value="Login"> Login </Button>
+        </Form>
+      <div className="pop-up-div">
+        <Popup trigger={<Button icon="add" />} header="King Sejong" content="The modern name for the Korean alphabet is Hangeul (or Hangul). King Sejong the Great is a celebrated Korean ruler who invented the alphabet. "
+          horizontalOffset={50}
+          position='right center' />
       </div>
-    </div>
-  );
+      </div>
+    </div>;
 }
 }
 
